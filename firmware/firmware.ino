22518d0872f6bf9aa56b921f20b2bb0e3fcc1543
5shekel <yair99@gmail.com>
2019-02-15 14:46:37 +0100

    purr-data patch

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,6 +2,7 @@
 
 // based on QTRSrawValues example code
 #include <QTRSensors.h>
+
 #define NUM_SENSORS   8     // number of sensors used
 #define TIMEOUT       2500  // waits for 2500 microseconds for sensor outputs to go low
 #define EMITTER_PIN   QTR_NO_EMITTER_PIN     // emitter is controlled by digital pin 2
@@ -13,13 +14,14 @@ QTRSensorsRC qtrrc((unsigned char[]) {
 NUM_SENSORS, TIMEOUT, EMITTER_PIN);
 unsigned int sensorValues[NUM_SENSORS];
 
-char bin[8] = "11111010";
-
+#include "table.h"
+int prevOut;
 
 void setup()
 {
   Serial.begin(115200);
   delay(100);
+
 }
 
 
@@ -36,13 +38,56 @@ void loop()
       sensorValues[i] = 0;
   }
 
-  //magic line of the interwebs ! 
+  //magic line of the interwebs !
   //convert binary '00001111' to int
   for (int ii = 0; ii < 8; ii++) {
     noOut = noOut | ((sensorValues[ii] + '0') - '0') << (7 - ii);
   };
 
-  Serial.print(noOut); Serial.println();
+  //generate the table
 
+  if (noOut != prevOut) {
+    if (noOut == 0) {
+      Serial.println("start");
+      Serial.println("0");
+    } else {
+      Serial.println(noOut);
+    }
+  }
+  prevOut = noOut;
+  /*
+    Serial.print(noOut);
+    Serial.print("\t");
+    Serial.print(grayTable[noOut]);
+    Serial.println();
+  */
   delay(5);
+}
+
+/*
+   This function converts an unsigned binary
+   number to reflected binary Gray code.
+
+   The operator >> is shift right. The operator ^ is exclusive or.
+*/
+unsigned int BinaryToGray(unsigned int num)
+{
+  return num ^ (num >> 1);
+}
+
+/*
+   This function converts a reflected binary
+   Gray code number to a binary number.
+   Each Gray code bit is exclusive-ored with all
+   more significant bits.
+*/
+unsigned int GrayToBinary(unsigned int num)
+{
+  unsigned int mask = num >> 1;
+  while (mask != 0)
+  {
+    num = num ^ mask;
+    mask = mask >> 1;
+  }
+  return num;
 }
